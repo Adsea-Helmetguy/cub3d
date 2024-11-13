b3569d2ac4afd867f8a933092ef4bc0a7fc885b7
mlow <marcuslowsj@gmail.com>
2024-11-13 14:33:38 +0800

    ready to sumbit

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@
 
 NAME = cub3D
 
-CC = cc -g
+CC = cc
 CFLAGS = -Wall -Werror -Wextra -g
 
 SRC_DIR = ./main_src
--- includes/cub3D.h
+++ includes/cub3D.h
@@ -77,120 +77,110 @@ typedef struct s_getcolor
 
 }					t_getcolor;
 
-typedef struct s_element // the data structure
+typedef struct s_element
 {
-	// stores the data of
-	// NO
-	char *north_texture;
-	// SO
-	char *south_texture;
-	// WE
-	char *west_texture;
-	// EA
-	char *east_texture;
-	// F
-	int floor_color[3]; // R,G,B colors in range [0,255]: 0, 255, 255
-	// C
-	int ceiling_color[3]; // R,G,B colors in range [0,255]: 0, 255, 255
-}					t_element;
+	char	*north_texture;
+	char	*south_texture;
+	char	*west_texture;
+	char	*east_texture;
+	int		floor_color[3];
+	int		ceiling_color[3];
+}				t_element;
 
-typedef struct s_data // the data structure
+typedef struct s_data
 {
-	int is_map_valid; // checking if map is even valid
-	int all_correct_elements;
-	char **map2d; // the map
-	int map_w;    // map width/later player location
-	int map_h;    // map height/later player location
-	double map_w_in_pixels;
-	double map_h_in_pixels;
-	int fd;
-	double distance;
-	int flag;
-	double radfov;
-}					t_data;
+	int		is_map_valid;
+	int		all_correct_elements;
+	char	**map2d;
+	int		map_w;
+	int		map_h;
+	double	map_w_in_pixels;
+	double	map_h_in_pixels;
+	int		fd;
+	double	distance;
+	int		flag;
+	double	radfov;
+}				t_data;
 
 typedef struct s_player
 {
-	char direction; // start direction
-	int pixel_x;    // location of player in x in INT
-	int pixel_y;    // location of player in y in INT
-	double p_x;     // player x(width) position in the map
-	double p_y;     // player y(height) position in the map
-	double dir_x;   // where the direction player faces in x
-	double dir_y;   // where the direction player faces in y
-	double plane_x; // the plane in x
-	double plane_y; // the plane in y
-	double			sidedir_x;
-	double			sidedir_y;
-	int				step_x;
-	int				step_y;
-	//	int		steps_taken;
-	int rotation;   // rotation flag
-	int left_right; // left right flag
-	int up_down;    // up down flag
-	double angle;   // player angle
-	void			*start;
-	void			**location;
-	double			hx;
-	double			hy;
-	double			vx;
-	double			vy;
-}					t_player;
+	char	direction;
+	int		pixel_x;
+	int		pixel_y;
+	double	p_x;
+	double	p_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	sidedir_x;
+	double	sidedir_y;
+	int		step_x;
+	int		step_y;
+	int		rotation;
+	int		left_right;
+	int		up_down;
+	double	angle;
+	void	*start;
+	void	**location;
+	double	hx;
+	double	hy;
+	double	vx;
+	double	vy;
+}				t_player;
 
-typedef struct s_raycasting // the ray structure
+typedef struct s_raycasting
 {
-	double camera_x;
-	double dir_x;
-	double dir_y;
-	int map_w;
-	int map_h;
-	double deltadist_x;
-	double deltadist_y;
-	int p_x; // player x(width) position in the map
-	int p_y; // player y(height) position in the map
-	int hit_side;
-	double perpwalldist;
-	int line_height;
-	int draw_start;
-	int draw_end;
-	double wall_x;
+	double	camera_x;
+	double	dir_x;
+	double	dir_y;
+	int		map_w;
+	int		map_h;
+	double	deltadist_x;
+	double	deltadist_y;
+	int		p_x;
+	int		p_y;
+	int		hit_side;
+	double	perpwalldist;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+	double	wall_x;
 
-}					t_raycasting;
+}				t_raycasting;
 
 typedef struct s_key
 {
-	int				map_x;
-	int				map_y;
-	int				w_pressed;
-	int				a_pressed;
-	int				s_pressed;
-	int				d_pressed;
-	int				left_rotate;
-	int				right_rotate;
-	//	int		toggle_state;
-	//	int		keystate[65600];
-}					t_key;
+	int		map_x;
+	int		map_y;
+	int		w_pressed;
+	int		a_pressed;
+	int		s_pressed;
+	int		d_pressed;
+	int		left_rotate;
+	int		right_rotate;
+}				t_key;
 
 typedef struct s_mlx
 {
-	void			*mlx_ptr;
-	void			*win_ptr;
-	void			*img_ptr;
-	char			*addr;
-	int				bits_per_pixel;
-	int				line_length;
-	int				endian;
-	int				color;
-}					t_mlx;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		color;
+}				t_mlx;
 
 typedef struct s_img
 {
-	void			*img;
-	int				*addr;
-	int				pixel_bits;
-	int				size_line;
-	int				endian;
-}					t_img;
+	void	*img;
+	int		*addr;
+	int		pixel_bits;
+	int		size_line;
+	int		endian;
+}				t_img;
 
 typedef struct s_game
 {
@@ -201,125 +191,103 @@ typedef struct s_game
 	t_key			key;
 	t_mlx			mlx;
 	t_img			img;
-	// images for textures
 	int				**textures;
-	int texture_width[4];  //*texture_width;
-	int texture_height[4]; //*texture_height
-							//
+	int				texture_width[4];
+	int				texture_height[4];
 	int				error_code;
 	int				screen_x;
 	int				screen_y;
 }					t_game;
 
 // main.c
-int					start_the_game(char **argv);
+int			start_the_game(char **argv);
 
 // testmap.c
-void				open_testmap(t_game *game, char *map_path);
+void		open_testmap(t_game *game, char *map_path);
 
 // check.c
-int					check_cub_valid_or_not(t_game *game, char **readmap);
+int			check_cub_valid_or_not(t_game *game, char **readmap);
 
 // checkmap.c
-int				checkmap_valid(t_game *game, char **data_map2d);
-char			*ft_strdup_checkmap(const char *s);
-char			**tmp_map2_helper(t_game *game, char **tmp_map);
+int			checkmap_valid(t_game *game, char **data_map2d);
+char		*ft_strdup_checkmap(const char *s);
+char		**tmp_map2_helper(t_game *game, char **tmp_map);
 
 // fill_space.c
-int					fill_space_tab(t_game *game, char ***tmp_map);
-char				**fill_space_void(t_game *game, char **map);
+int			fill_space_tab(t_game *game, char ***tmp_map);
+char		**fill_space_void(t_game *game, char **map);
 
 // floodfill.c
-void				map_floodfill(char ***tmp_map, t_game *game, int p_x,
-						int p_y);
-void				map_floodfill2(char ***tmp_map, t_game *game, int p_x,
-						int p_y);
-void				map_floodfill_checker(char ***tmp_map, t_game *game,
-						int p_x, int p_y);
+void		map_floodfill(char ***tmp_map, t_game *game, int p_x, int p_y);
+void		map_floodfill2(char ***tmp_map, t_game *game, int p_x, int p_y);
+void		map_floodfill_checker(char ***tmp_map, t_game *game, \
+				int p_x, int p_y);
 
 // freeing_struc.c
-void				free_textures(int **game_textures);
-void				ftps_free(char **to_free);
+void		free_textures(int **game_textures);
+void		ftps_free(char **to_free);
 
 // get_variables.c
-char				*get_variable_cub(t_game *game);
-void				get_variable_map(t_game *game, char **split_map);
-int					get_variable_element(t_game *game, char **readmap);
+char		*get_variable_cub(t_game *game);
+void		get_variable_map(t_game *game, char **split_map);
+int			get_variable_element(t_game *game, char **readmap);
 
 // search_value_color.c
-int					getting_color(char **map, char *var_name, int color[3]);
-char				*search_for_value(char **split_map, char *var_name);
+int			getting_color(char **map, char *var_name, int color[3]);
+char		*search_for_value(char **split_map, char *var_name);
 
 // init_variables.c
-void				init_variables(t_game *game);
-void				init_variables2(t_game *game);
-void				init_variable_player(t_game *game);
+void		init_variables(t_game *game);
+void		init_variables2(t_game *game);
+void		init_variable_player(t_game *game);
 
-//------------------------------void	render(t_game *game, int ray,
-							//----------------------------------------------------------
-							//--------new additions----------
-							// window_screen_creation.c
-							void
-							window_screen_creation(t_game *game);
+// window_screen_creation.c
+void		window_screen_creation(t_game *game);
 // texture.c
-void				setup_texture(t_game *game);
-void				init_texture_img(t_game *game, t_img *tmp, char *path,
-						int face);
+void		setup_texture(t_game *game);
+void		init_texture_img(t_game *game, t_img *tmp, char *path, int face);
 // mousekey_hook.c
-int					keyhook(int keycode, t_game *game);
-int					keyhook_release(int keycode, t_game *game);
-void				khook(t_game *game, double move_x, double move_y);
-void				move_player(t_game *game, double move_x, double move_y);
-void				rotate_player(t_game *game, int i);
+int			keyhook(int keycode, t_game *game);
+int			keyhook_release(int keycode, t_game *game);
+void		khook(t_game *game, double move_x, double move_y);
+void		move_player(t_game *game, double move_x, double move_y);
+void		rotate_player(t_game *game, int i);
 // game
-int					gameplay(t_game *game);
-//--------new additions----------
-//----------------------------------------------------------
-//----------------------------------------------------------
+int			gameplay(t_game *game);
 
 // ft_strcmp.c
-int					ft_strcmp(char *s1, char *s2);
+int			ft_strcmp(char *s1, char *s2);
 
 // print_msg
-void				print_msg(char *message, int exit_code);
+void		print_msg(char *message, int exit_code);
 
 // exit_function.c
-void				free_gameloop_end(char *message, int exit_code,
-						t_game *game);
-void				free_end_exit(char *message, int exit_code, t_game *game,
-						char **str);
-void				free_before_game(char *message, int exit_code,
-						t_game *game);
+void		free_gameloop_end(char *message, int exit_code, t_game *game);
+void		free_end_exit(char *message, int exit_code, \
+				t_game *game, char **str);
+void		free_before_game(char *message, int exit_code, t_game *game);
 
 // exit_utils.c
-int					x_close_window(t_game *game);
-int					close_window_invalidtextures(t_game *game);
-int					game_checkerror_exit(char *message, t_game *game);
-int					array_free_exit(char *message, t_game *game, char ***to_free);
+int			x_close_window(t_game *game);
+int			close_window_invalidtextures(t_game *game);
+int			game_checkerror_exit(char *message, t_game *game);
+int			array_free_exit(char *message, t_game *game, char ***to_free);
 
 // mlxpixel.c
-void				mlxpixel(t_game *game, int x, int y, int color);
-void				mlxpixel_on_screen(t_game *game);
+void		mlxpixel(t_game *game, int x, int y, int color);
+void		mlxpixel_on_screen(t_game *game);
 
 // raycasting.c
-void				raycasting(t_game *game);
-int					x_positive(double angle);
-int					y_positive(double angle);
-double				nor_angle(double angle);
-void				render(t_game *game, int ray, double angle);
+void		raycasting(t_game *game);
+int			x_positive(double angle);
+int			y_positive(double angle);
+double		nor_angle(double angle);
+void		render(t_game *game, int ray, double angle);
 
 // ray_utils.c
-int					init_h(double angle, double *x_step, double *y_step, double *y_intercept);
-int					init_v(double angle, double *x_step, double *y_step, double *x_intercept);
+int			init_h(double angle, double *x_step, double *y_step, \
+				double *y_intercept);
+int			init_v(double angle, double *x_step, double *y_step, \
+				double *x_intercept);
 
 #endif
-/*
-#include <errno.h> // for perror()
-#include <fcntl.h> //open()
-#include <stdio.h> // for printf()
-#include <stdlib.h> // exit()
-#include <sys/types.h> //for wait() / fork() / pid_t() / wait() / waitpid()
-#include <sys/wait.h> // wait() / waitpid()
-#include <unistd.h> // for fork() / execve() / access()
-
-*/
